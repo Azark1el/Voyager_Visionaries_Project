6181808533229fefb2339cb153a6977a2184cbe9
ThomasdeCarpio <nguyenhthang2005a@gmail.com>
2023-10-08 14:39:05 +0700

    Web again

--- Web Backup/spaceapp/urls.py
+++ Web Backup/spaceapp/urls.py
@@ -4,11 +4,13 @@ from . import views
 #page urls
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('Home/', views.home, name='Home'),
     path('About/', views.about, name='About'),
-    path('About/', views.aboutrdr, name='About'),
     path('More/', views.more, name = 'More'),
     path('Data/', views.data, name = 'Data'),
-    path('Explore/', views.explore, name = 'Explore')
-    
+    path('Explore/', views.explore, name = 'Explore'),
+
+    # redirect
+
+    path('Home/', views.home, name='Home'),
+    path('About/', views.about_rdr, name='About')
 ]
