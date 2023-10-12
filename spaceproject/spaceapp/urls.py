320aa66e9ebe52201e4ec6eeda4fdf13c7c8c2bf
ThomasdeCarpio <nguyenhthang2005a@gmail.com>
2023-10-12 11:50:27 +0700

    urls fix

--- spaceproject/spaceapp/urls.py
+++ spaceproject/spaceapp/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('About/', views.about, name='About'),
-    path('More/', views.more, name = 'More'),
     path('Data/', views.data, name = 'Data'),
     path('Explore/', views.explore, name = 'Explore'),
     path('Debris/', views.debris, name = 'Debris'),
@@ -16,6 +15,7 @@ urlpatterns = [
     path('Home/', views.home_rdr, name='Home'),
     path('About/', views.about_rdr, name='About'),
     path('Data/', views.data_rdr, name='Data'),
+    path('Data/', views.explore_rdr, name='Data'),
     path('Debris/', views.debris_rdr, name='Debris'),
     path('Weather/', views.weather_rdr, name = 'Weather')
 ]
